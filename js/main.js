c3ba34b34d07f3d3cd0d577f4659e7866ca24062
Oksana Astapova <astapova.frontend@gmail.com>
2022-06-14 09:14:32 +0300

    refactor: clean js files

--- js/main.js
+++ js/main.js
@@ -104,3 +104,224 @@ document.querySelectorAll("li").forEach(k => {
     })
 })
 
+/*-------PROJECTS-------------*/
+
+let keyboard = document.querySelector('.page__button_keyboard');
+let shelter = document.querySelector('.page__button_shelter');
+let mem = document.querySelector('.page__button_mem');
+let screenKeyboard = document.querySelector('.screen_keyboard');
+let screenShelter = document.querySelector('.screen_shelter');
+let screenMem = document.querySelector('.screen_mem');
+
+keyboard.addEventListener('click', () => {
+   keyboard.classList.add('page__button_active'); 
+   shelter.classList.remove('page__button_active');
+   mem.classList.remove('page__button_active');
+
+   screenKeyboard.classList.add('screen_active');
+   screenShelter.classList.remove('screen_active');
+   screenMem.classList.remove('screen_active'); 
+})
+
+shelter.addEventListener('click', () => {
+   shelter.classList.add('page__button_active'); 
+   keyboard.classList.remove('page__button_active');
+   mem.classList.remove('page__button_active');
+   
+   screenShelter.classList.add('screen_active'); 
+   screenKeyboard.classList.remove('screen_active');
+   screenMem.classList.remove('screen_active');
+})
+
+mem.addEventListener('click', () => {
+    mem.classList.add('page__button_active'); 
+    keyboard.classList.remove('page__button_active');
+    shelter.classList.remove('page__button_active');
+ 
+    screenMem.classList.add('screen_active'); 
+    screenShelter.classList.remove('screen_active');
+    screenShelter.classList.remove('screen_active');
+})
+
+document.querySelector(".menu__button_projects").addEventListener("click", ()=>{
+
+   document.querySelector(".go-project_right_keyboard").classList.add("transition-right");
+   document.querySelector(".go-project_right_mem").classList.add("transition-right");
+   document.querySelector(".go-project_left").classList.add("transition-left");
+})
+
+document.querySelectorAll(".menu__button_click").forEach(k => {
+
+    k.addEventListener("click", ()=>{
+
+      document.querySelector(".go-project_right_keyboard").classList.remove("transition-right");
+      document.querySelector(".go-project_right_mem").classList.remove("transition-right");
+      document.querySelector(".go-project_left").classList.remove("transition-left");
+    })
+})
+
+/*-------INSPIRATION-------------*/
+
+const cardsData = [
+    {"name": "1",
+    "img": "assets/pict-inspiration/1.jpg",
+    "alt": "Fairyland"},
+
+    {"name": "2",
+    "img": "assets/pict-inspiration/2.jpg",
+    "alt": "Mistery nature"},
+
+    {"name": "3",
+    "img": "assets/pict-inspiration/3.jpg",
+    "alt": "Adorable sunset with butterfly"},
+
+    {"name": "4",
+    "img": "assets/pict-inspiration/4.jpg",
+    "alt": "Dew colorfull leaves"},
+
+    {"name": "5",
+    "img": "assets/pict-inspiration/5.jpg",
+    "alt": "Wonderful autumn nature"},
+
+    {"name": "6",
+    "img": "assets/pict-inspiration/6.jpg",
+    "alt": "A girl with the sunset"},
+
+    {"name": "7",
+    "img": "assets/pict-inspiration/7.jpg",
+    "alt": "Adorable yellow flower"},
+
+    {"name": "8",
+    "img": "assets/pict-inspiration/8.jpg",
+    "alt": "Awsome lavender field"},
+
+    {"name": "9",
+    "img": "assets/pict-inspiration/9.jpg",
+    "alt": "Mistery double rainbow"},
+
+    {"name": "10",
+    "img": "assets/pict-inspiration/10.jpg",
+    "alt": "Stone towers by the sea"},
+
+    {"name": "11",
+    "img": "assets/pict-inspiration/11.jpg",
+    "alt": "Wonderful flower field"},
+
+    {"name": "12",
+    "img": "assets/pict-inspiration/12.jpg",
+    "alt": "Pink clouds"},
+
+    {"name": "13",
+    "img": "assets/pict-inspiration/13.jpg",
+    "alt": "Adorable sunset over the pier"},
+
+    {"name": "14",
+    "img": "assets/pict-inspiration/14.jpg",
+    "alt": "Wonderful colorfull rose"},
+
+    {"name": "15",
+    "img": "assets/pict-inspiration/15.jpg",
+    "alt": "Awsome water droplets butterfly"},
+
+    {"name": "16",
+    "img": "assets/pict-inspiration/16.jpg",
+    "alt": "Mistery pink nature"},
+
+    {"name": "17",
+    "img": "assets/pict-inspiration/17.jpg",
+    "alt": "Wonderful soap bubble"},
+
+    {"name": "18",
+    "img": "assets/pict-inspiration/18.jpg",
+    "alt": "Mistery nature"},
+
+]
+
+let k;
+let wrapper = document.querySelector(".gallery");
+
+document.addEventListener("DOMContentLoaded", () => {
+    for(let k=0; k<18; k++){
+        let card = `<div class = "gallery__card" onclick="openModal(${k})">
+        <img src="${cardsData[k].img}" alt="${cardsData[k].alt}">
+        </div>`;
+
+        wrapper.innerHTML += card;
+    }
+})
+
+
+/*------MODAL CARDS--------*/
+
+let modal = document.querySelector(".modal-card");
+let blackBack = document.querySelector(".black-back");
+let cancel = document.querySelector(".modal-card__close-btn");
+let body = document.querySelector(".body");
+let modalWrapper = document.querySelector("modal-card__wrapper");
+
+function openModal(k){
+
+    blackBack.style.visibility = "visible";
+    modal.style.visibility = "visible";
+    modal.style.opacity = "1";
+    body.style.overflow = "hidden";
+        modal.classList.add('animation-modal');
+
+
+    let modalWrapper = `<button class="modal-card__close-btn" onclick="closeModal()">
+                <img src="assets/icons/close-btn.png" alt="close button">
+                        </button>
+                        <div class="modal-card__wrapper">
+                <img src="${cardsData[k].img}" alt="${cardsData[k].alt}"> 
+                        </div>`;
+                       
+
+        modal.innerHTML += modalWrapper;
+       
+}
+
+function closeModal(){
+    modal.innerHTML = "";
+    blackBack.style.visibility = "hidden";
+    modal.style.visibility = "hidden";
+    modal.style.opacity - "0";
+    body.style.overflow = "visible";
+    
+}
+
+/*-------CONTACTS-------------*/
+
+let callMe_btn = document.querySelector(".call-me__btn");
+let callMe_content = document.querySelector(".call-me__content");
+let callYou_btn = document.querySelector(".call-you__btn");
+let callYou_content = document.querySelector(".contact-form");
+let callMe_cancel = document.querySelector(".call-me__cancel");
+let callYou_cancel = document.querySelector(".call-you__cancel");
+
+callMe_btn.addEventListener("click",()=>{
+
+    callMe_content.style.visibility = "visible";
+    callMe_content.style.opacity = "1";
+
+})
+
+
+callYou_btn.addEventListener("click",()=>{
+
+    callYou_cancel.style.display = "flex";
+    callYou_content.style.visibility = "visible";
+    callYou_content.style.opacity = "1";
+
+})
+
+callMe_cancel.addEventListener("click", ()=>{
+    callMe_content.style.visibility = "hidden";
+    callMe_content.style.opacity = "0";  
+    
+})
+
+callYou_cancel.addEventListener("click", ()=>{
+    callYou_content.style.visibility = "hidden";
+    callYou_content.style.opacity = "0";  
+    
+})
